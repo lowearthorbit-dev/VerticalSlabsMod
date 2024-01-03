f6374489b8adc40593c592ea9c1f03c14ea5755b
Brainterminator <thebrainterminator@gmail.com>
2024-01-03 18:35:43 +0100

    -implementing DataGen

---
+++ src/main/java/com/brainterminator/verticalslabs/data/ModBlockTagProvider.java
@@ -0,0 +1,23 @@
+package com.brainterminator.verticalslabs.data;
+
+import com.brainterminator.verticalslabs.VerticalSlabs;
+import com.brainterminator.verticalslabs.handler.VerticalSlabGenerator;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagProvider extends BlockTagsProvider {
+    public ModBlockTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, VerticalSlabs.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        //this.tag(BlockTags.MINEABLE_WITH_AXE).add(VerticalSlabGenerator.OAK_VERTICAL_SLAB.get());
+    }
+}
