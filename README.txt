e2a66e33de4b84ff03eae2ab5b3098805c920969
Brainterminator <thebrainterminator@gmail.com>
2024-01-03 14:46:23 +0100

    Init

---
+++ README.txt
@@ -0,0 +1,2 @@
+# Vertical Slabs
+## by Brainterminator
---
+++ src/main/java/com/brainterminator/verticalslabs/handler/VerticalSlabGenerator.java
@@ -0,0 +1,71 @@
+package com.brainterminator.verticalslabs.handler;
+
+import com.brainterminator.verticalslabs.VerticalSlabs;
+import com.brainterminator.verticalslabs.blocks.VerticalSlabBlock;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class VerticalSlabGenerator {
+
+    // Create a Deferred Register to hold Blocks which will all be registered under the "verticalslabs" namespace
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, VerticalSlabs.MODID);
+    // Create a Deferred Register to hold Items which will all be registered under the "verticalslabs" namespace
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, VerticalSlabs.MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "verticalslabs" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, VerticalSlabs.MODID);
+
+
+    //ADD BLOCKS:
+
+    static {
+        //LOAD SLABS FROM VANILLA MINECRAFT
+        Class<?> blocks = Blocks.class;
+        ArrayList<Block> slabs = new ArrayList<>();
+        Field[] fields = blocks.getFields();
+        for (Field field : fields) {
+            if (field.getName().contains("_SLAB")) {
+                try {
+                    slabs.add((Block)field.get(null));
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        //GENERATE ACCORDING SLAB BLOCKS
+        for(Block slab: slabs){
+            registerWall(slab);
+        }
+    }
+
+
+    // Registers the creative tab and adds all items
+    public static final RegistryObject<CreativeModeTab> VERTICAL_SLABS_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> Items.OAK_SLAB.getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                for (RegistryObject<Item> item : ITEMS.getEntries()) {
+                    output.accept(item.get());
+                }
+            }).build());
+
+    // Creates and registers a vertical slab from an existing Block
+    private static RegistryObject<Block> registerWall(Block block) {
+        String name = block.getName().toString();
+        name = name.split("'")[1].replace("_slab", "_vertical_slab").replace("block.minecraft.","");
+        System.out.println(name);
+        RegistryObject<Block> toReturn = BLOCKS.register(name,
+                () -> new VerticalSlabBlock(() -> block.defaultBlockState(),
+                        BlockBehaviour.Properties.copy(block)));
+        ITEMS.register(name, () -> new BlockItem(toReturn.get(), new Item.Properties()));
+        return toReturn;
+    }
+}
